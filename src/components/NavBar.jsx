4f58388311c2b90111553fccb8608e8c92027638
DarkEngelBlood <darkengelblood@gmail.com>
2024-09-05 23:24:52 -0300

    EntregaFinal+Rodriguez

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,35 +1,38 @@
-import { Link, NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import { CartWidget } from "./CartWidget"
 import styles from "./Navbar.module.css"
 
 export const NavBar = () => {
   return (
-    <div className={styles.navbar}>
-      <div>
+    <nav className={styles.navbar}>
+      <div className={styles.brand}>
         <h1>Mimic</h1>
       </div>
-      <div>
+      <div className={styles.navLinks}>
         <ul className={styles.ul}>
+          <NavLink to="/" className={`${({ isActive }) => (isActive ? styles.active : styles.navLink)} ${styles.homeNav}`}>
+            <p className={styles.home}>Inicio</p>
+          </NavLink>
           <li className={styles.list}>
-            <Link as={NavLink} to="/" className={styles.a}>
-              Inicio
-            </Link>
-          </li>
-          <li className={styles.list}>
-            <Link as={NavLink} to="/category/Figura" className={styles.a}>
-              Figuras
-            </Link>
-          </li>
-          <li className={styles.list}>
-            <Link as={NavLink} to="/category/Manga" className={styles.a}>
-              Mangas
-            </Link>
+            <div className={styles.dropdown}>
+              <div className={styles.dropdownButton}>
+                Categorías
+              </div>
+              <div className={styles.dropdownContent}>
+                <NavLink to="/category/Figura" className={({ isActive }) => (isActive ? styles.active : styles.navLink)}>
+                  Figuras
+                </NavLink>
+                <NavLink to="/category/Manga" className={({ isActive }) => (isActive ? styles.active : styles.navLink)}>
+                  Mangas
+                </NavLink>
+              </div>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
+      <div className={styles.cart}>
         <CartWidget />
       </div>
-    </div>
+    </nav>
   )
 }
